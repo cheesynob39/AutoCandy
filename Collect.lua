6f89ebb9d938c1d66a73dbe47976236564e530d4
Caspar <111374516+cheesynob39@users.noreply.github.com>
2022-10-04 21:03:18 +0100

    Update Collect.lua

--- Collect.lua
+++ Collect.lua
@@ -1,46 +1,5 @@
-if game.PlaceId == nil then
-    
-    
-    else
+if 7< game.Players:GetChildren() then
 
-        repeat wait(2) until game:IsLoaded()
-
-print("Credits to CypherM#8368 for the autorejoin")
-
-while true and wait(.25) do
-if game.CoreGui:FindFirstChild('RobloxPromptGui') then
- 
-local PromptGui,TeleportService = game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService"TeleportService"
- 
-PromptGui.ChildAdded:connect(function(find)
-    if find.Name == 'ErrorPrompt' then
-        repeat
-            TeleportService:Teleport(game.PlaceId)
-            wait(2)
-        until false
-    end
-end)
-end
-
-wait()
-
-    while true do 
-    wait(.25) 
-    for i,v in pairs(game.workspace.CandyCorns:GetDescendants()) do 
-    if v:IsA('Part') and v:GetChildren() then 
-    wait(2.5)
-    game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2, Enum.EasingStyle.Sine, Enum.EasingDirection.Out),{CFrame = v.CFrame}):Play() 
-    
-     end 
-    end
-     end 
-    end
-
-    while true do
-    wait(.25)
-if game.Players:GetChildren() 7< then
-    
-    
 local PlaceID = game.PlaceId
 local AllIDs = {}
 local foundAnything = ""
@@ -111,9 +70,45 @@ function Teleport()
 end
 
 Teleport()
-
-end
     end
+
+wait(5)
+
+if game.PlaceId == nil then
+    
+    
+    else
+
+        repeat wait(2) until game:IsLoaded()
+
+print("Credits to CypherM#8368 for the autorejoin")
+
+while true and wait(.25) do
+if game.CoreGui:FindFirstChild('RobloxPromptGui') then
+ 
+local PromptGui,TeleportService = game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService"TeleportService"
+ 
+PromptGui.ChildAdded:connect(function(find)
+    if find.Name == 'ErrorPrompt' then
+        repeat
+            TeleportService:Teleport(game.PlaceId)
+            wait(2)
+        until false
+    end
+end)
 end
 
+wait()
+
+    while true do 
+    wait(.25) 
+    for i,v in pairs(game.workspace.CandyCorns:GetDescendants()) do 
+    if v:IsA('Part') and v:GetChildren() then 
+    wait(2.5)
+    game:GetService('TweenService'):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2, Enum.EasingStyle.Sine, Enum.EasingDirection.Out),{CFrame = v.CFrame}):Play() 
+    
+     end 
+    end
+     end 
+    end
 
